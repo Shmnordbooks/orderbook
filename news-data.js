f2ab923a88f595dd94d23ec81327140b6e39228e
Shmnordbooks <tuncay.gecim@shimano-eu.com>
2026-03-31 17:39:18 +0300

    Add files via upload

---
+++ news-data.js
@@ -0,0 +1,25 @@
+const SHIMANO_NEWS = [
+  
+  {
+    title: "History of INNOVATION",
+    date: "31-03-2026",
+    excerpt: "Sound ideas forge a new standard of quality and performance",
+    img: "https://www.shimano.com/jp/img/header/Innovation_en@2x.png",
+    link: "https://www.shimano.com/en/manufacturing/innovation.html"
+  },
+ 
+  {
+    title: "The All Bodies on Bikes Story ",
+    date: "MARCH 31, 2026",
+    excerpt: "There’s a line at the end of our first film All Bodies on Bikes, where I ask “So where does this conversation go from here?.",
+    img: "https://bike.shimano.com/content/dam/one-website/common/stories/we-are-cyclist-/We-Are-Cyclist-Hero-Desk.jpg", 
+    link: "https://bike.shimano.com/en-NA/stories/article/we-are-cyclists.html"
+  },
+  {
+    title: "SPONSORED ROAD TEAMS OF 2026",
+    date: "MARCH 31, 2026",
+    excerpt: "We are committed to supporting professional cycling at all levels, from the races in your backyard to the UCI WorldTour and everything in between.",
+    img: "https://bike.shimano.com/content/dam/one-website/common/stories/sponsored-road-teams-of-2026/FullMediaBanner_Sponsored-Road-2026_PC_1440x720px.jpg",
+    link: "https://bike.shimano.com/en-NA/stories/article/sponsored-road-teams-of-2026.html"
+  }
+];
