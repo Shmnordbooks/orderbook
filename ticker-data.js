163a624a2e60bf35222d6ac2ee9ec01bb0150a1c
Shmnordbooks <tuncay.gecim@shimano-eu.com>
2026-05-12 17:14:34 +0300

    Add files via upload

--- ticker-data.js
+++ ticker-data.js
@@ -11,14 +11,16 @@ window.SHIMANO_TICKER = [
     title: "---Welcome to New Shimano Order Book Webpage ---",
     added: "2026-04-22"
   },
-   {
-    title: "---New Flat Pedals \" PD-G8040\" and \"PD-M8240\" and Flat/SPD  Pedal \"PD-EH510\" added in PEDALS page---",
-    added: "2026-04-22"
-  },
 
   {
     title: "---Dear partners, please note that our office will be closed between 25-29 May for Eid al-Adha. Our operations will resume on 1th June.---",
     added: "2026-05-12"
   },
+
+   {
+    title: "---New Flat Pedals \" PD-G8040\" and \"PD-M8240\" and Flat/SPD  Pedal \"PD-EH510\" added in PEDALS page---",
+    added: "2026-04-22"
+  },
+
 
 ];
